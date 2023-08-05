43dd01ab73211ff71d387e982e98082c869e221c
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-08-04 17:27:11 -0700

    add route structure

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,7 @@
-const express = require('express')
+const router = require('express').Router();
 
-const router = express.Router()
+router.route('/').get().post();
+router.route('/:contactId').get().delete().put();
+router.route('/:contactId/favorite').patch();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
